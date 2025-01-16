931addce27a78490f9a2f0aa2063435fe2155661
Siddharth <siddharth@vasantsoftwaressolution.com>
2025-01-16 18:38:20 +0530

    initialize Prisma setup with User and Post models, services, and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './services/prisma.service';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
